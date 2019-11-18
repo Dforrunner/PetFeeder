dbf64c904d0e33473fdd628d81beec1c76ab1b18
Dforrunner <vtme996@gmail.com>
2019-11-18 18:11:34 -0500

    changed gpio pin

--- rpi_controllers/run_motor.py
+++ rpi_controllers/run_motor.py
@@ -6,12 +6,12 @@ def run_motor():
     pin = 14
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setup(pin, GPIO.IN)
 
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(pin, GPIO.LOW)
     print('feeding...')
     time.sleep(5)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH)
     print('complete!')
     GPIO.cleanup()
     return None
